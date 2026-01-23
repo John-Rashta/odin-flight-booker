81e5dbf4b845eefe57361e871838d2d3ad6889fd
John-Rashta <john_rashta@outlook.com>
2026-01-23 20:57:43 +0000

    feat: start work on getting flights

---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,20 @@
+class FlightsController < ApplicationController
+  def index
+    filtered_params = index_params if params[:start_time].presence
+    @found_flights = Flight.where(
+      departure_airport_id: filtered_params[0],
+      arrival_airport_id: filtered_params[1],
+      start_time: filtered_params[3].all_day
+    ) if filtered_params
+    @all_airports = Airport.pluck(:code, :id)
+    @all_dates = Flight.distinct.order(:start_time)
+  end
+
+  private
+
+  def index_params
+    filtered_params = params.expect(:departure_airport, :arrival_airport, :num_of_passengers, :start_time)
+    filtered_params[3] = Time.at(filtered_params[3].to_i).to_datetime
+    filtered_params
+  end
+end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,4 +3,12 @@ class Flight < ApplicationRecord
   validates :start_time, presence: true
   belongs_to :arrival_airport, class_name: "Airport"
   belongs_to :departure_airport, class_name: "Airport"
+
+  def start_time_formatted
+    start_time.strftime("%m/%d/%Y")
+  end
+
+  def start_time_seconds
+    start_time.to_time.to_i
+  end
 end
